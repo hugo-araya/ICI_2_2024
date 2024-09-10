2c17ab31b752862a013ff6937b0d2948c4d959e3
Hugo Araya C. <42989121+hugo-araya@users.noreply.github.com>
2024-09-10 09:05:03 -0300

    Update 05_kaprekar_5.py

--- ejrcicios_pr/05_kaprekar_5.py
+++ ejrcicios_pr/05_kaprekar_5.py
@@ -1,5 +1,13 @@
+# [82962, 75933, 63954, 61974], [74943, 62964, 71973, 83952] y [53955, 59994]
+
+def ciclos():
+    ciclo1 = [82962, 75933, 63954, 61974]
+    ciclo2 = [74943, 62964, 71973, 83952]
+    ciclo3 = [53955, 59994]
+    return ciclo1, ciclo2, ciclo3
+
 def lee_numero():
-    numero = '12345'
+    numero = '75933'
     return numero
 
 def da_vuelta(numero):
@@ -30,16 +38,23 @@ def diferencia (nume_1, nume_2):
     return dife
 
 def ciclo_kaprekar(nume_or):
+    c1, c2, c3 = ciclos()
     ciclo = []
     anterior = ''
     nume_calculado = nume_or
-    while anterior != nume_calculado:
+    while (nume_calculado not in c1) and (nume_calculado not in c2) and (nume_calculado not in c3):
         ciclo.append(nume_calculado)
         nume_1 = may_men(nume_calculado)
         nume_2 = men_may(nume_calculado)
         nume_1_2 = diferencia(nume_1, nume_2)
-        anterior = nume_calculado
+        #anterior = nume_calculado
         nume_calculado = nume_1_2
+    if nume_calculado in c1:
+        ciclo = c1[:]
+    elif nume_calculado in c2:
+        ciclo = c2[:]
+    else:
+        ciclo = c3[:]
     return nume_calculado, ciclo
 
 def mostrar_constante(constante, ciclo):
